f5a38796e55f7973f61b6bcfb07ae19422880edd
hoanglong212 <69975681+hoanglong212@users.noreply.github.com>
2026-02-16 00:31:34 +0700

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/react-blog/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )

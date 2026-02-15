ab9b2fadfcfede0206a90be811d4ab00b1a0015d
hoanglong212 <69975681+hoanglong212@users.noreply.github.com>
2026-02-16 00:52:22 +0700

    a

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/Finalterm/">
-    <App />
-  </BrowserRouter>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)

85fc3d831cb424d884f2140a90f856d5b95543e2
hoanglong212 <69975681+hoanglong212@users.noreply.github.com>
2026-02-16 02:50:23 +0700

    a

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,31 @@ import WriteBlog from './pages/Writeblog'
 import LoginPage from './pages/LoginPage'
 import { getUser } from './auth'
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <HomePage />,
-    },
-    {
-      path: '/explore',
-      element: <ExplorePage />,
-    },
-    {
-      path: '/login',
-      element: <LoginPage />,
-    },
-    {
-      path: '/write',
-      element: <WriteBlog />,
-      loader: () => {
-        const user = getUser()
-        if (!user) {
-          return redirect('/login')
-        }
-        return null
-      },
-    },
-  ],
+const router = createBrowserRouter([
   {
-    basename: '/Finalterm/',
-  }
-)
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/explore',
+    element: <ExplorePage />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  {
+    path: '/write',
+    element: <WriteBlog />,
+    loader: () => {
+      const user = getUser()
+      if (!user) {
+        return redirect('/login')
+      }
+      return null
+    },
+  },
+])
 
 function App() {
   return <RouterProvider router={router} />

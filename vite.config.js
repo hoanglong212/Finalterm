6c94b19c7419939abf4ec2e881eb7575e25f1506
hoanglong212 <69975681+hoanglong212@users.noreply.github.com>
2026-02-16 00:28:05 +0700

    change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/',
+  base: '/react-blog/', // 👈 sửa ở đây
   plugins: [
     react({
       babel: {

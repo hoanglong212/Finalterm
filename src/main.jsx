eec3cfee77c2884bc5a98526bf5e595c8fa3e601
hoanglong212 <69975681+hoanglong212@users.noreply.github.com>
2026-02-16 00:44:55 +0700

    a

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/Finalterm/">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename="/Finalterm/">
+    <App />
+  </BrowserRouter>
 )
